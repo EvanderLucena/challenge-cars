d582bdb95b2a598f2178057917a5a9e71715eee5
evand <evander.willian@gmail.com>
2024-07-02 14:49:21 -0300

    initial commit

---
+++ src/main/java/br/com/lucena/challengecars/ChallengeCarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucena.challengecars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeCarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChallengeCarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/lucena/challengecars/ChallengeCarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucena.challengecars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeCarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
